463cb4a0283f294afe7c536e1a9a6a8eed743467
KimSol <ksol92789@gmail.com>
2023-07-09 16:53:12 +0900

    Bug fix
    1. 빌드 오류 수정
    2. gitignore에 .vsconfig 추가

--- Source/ArenaBattle/Character/ABCharacterPlayer.cpp
+++ Source/ArenaBattle/Character/ABCharacterPlayer.cpp
@@ -6,6 +6,7 @@
 #include "Camera/CameraComponent.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
 
 AABCharacterPlayer::AABCharacterPlayer()
 {
